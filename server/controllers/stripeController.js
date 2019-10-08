7c3524323377cdc3e02991b4ca5236121ad86ae8
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-10-08 17:02:51 -0400

    stripe backend work

---
+++ server/controllers/stripeController.js
@@ -0,0 +1,32 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+module.exports = {
+  pay: (req, res) => {
+    const {
+      token: { id },
+      amount
+    } = req.body;
+    // console.log(id, amount, stripe)
+    stripe.charges.create(
+      {
+        amount: amount,
+        currency: "usd",
+        source: id,
+        description: "Test Charge"
+      },
+      (err, charge) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send(err);
+        } else {
+          console.log(
+            "Successful payment"
+            // , charge
+          );
+
+          return res.status(200).send(charge);
+        }
+      }
+    );
+  }
+};
